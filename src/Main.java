a730fd9b380437df34895e2cf5933bc0e1da0459
Izzan Fernández Lareu <a25izzanfl@teis.intranet>
2025-09-16 17:32:10 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,51 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Ejercicio 1
+        // Ejercicio 1 Bucle
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Ingresa un numero");
+        int N = sc.nextInt();
+        for ( int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+
+        // Ejercicio 2 Bucle
+
+        System.out.println("Ingresa un numero");
+        int N1 = sc.nextInt();
+        for (int i = N; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        // Ejercicio 3 Bucle
+
+        System.out.println("Ingresa un numero");
+        int numero = sc.nextInt();
+
+        System.out.println("Tabla de multiplicar del " + numero + ":");
+
+        for (int i =1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " - " + (numero * i));
+        }
+
+        // Ejercicio 4 Bucle
+
+        System.out.println("Ingresa un numero");
+        sc.nextLine();
+        int n = sc.nextInt();
+        int suma = 0;
+        for (int i = 1; i <= n; i++) {
+            suma += i;
+        }
+        System.out.println("La suma de los numeros del 1 al " + n + " es " + suma);
+
+        // Ejercicio 5 Bucle
+
+        System.out.println("Ingresa un numero");
+        int n1 = sc.nextInt();
+        int par =
 
     }
 }
