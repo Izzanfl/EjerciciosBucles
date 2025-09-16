b2801810fc807d17773a2a729a13b2859ad94a62
Izzan Fernández Lareu <a25izzanfl@teis.intranet>
2025-09-16 16:56:08 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Ejercicio 1
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa un numero");
+
+    }
+}
